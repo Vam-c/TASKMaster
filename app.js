1102e6ef8ba156d335051770815b5619249333a6
vamsee <vamsee.somasi@gmail.com>
2022-01-26 12:37:57 +0530

    Remove copyright, run on local server, add readme

--- app.js
+++ app.js
@@ -132,10 +132,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port || 3000, function() {
+
+app.listen(3000, function() {
   console.log("Server started successfully");
 });
